d60c93561211507835b550784451b431105cfe6a
Alex Tomkins <tomkins@darkzone.net>
2020-10-04 23:08:46 +0100

    Add support for Python 3.8
    Also upgrade our testing to use Python 3.8

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Django",
         "Framework :: Django :: 1.8",
         "Framework :: Django :: 1.11",
