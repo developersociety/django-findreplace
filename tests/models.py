2a32117314b843dca64b69ae7dc0c3d716a63683
Alex Tomkins <tomkins@darkzone.net>
2024-12-22 17:34:25 +0000

    Update support to Django 3.2+ and Python 3.9+

--- tests/models.py
+++ tests/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     url = models.URLField("URL")
--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,7 @@
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 
+USE_TZ = True
+
 SECRET_KEY = "findreplace"
 
 INSTALLED_APPS = ["findreplace", "tests"]
